e94cf4f11e9b92c46160075d26c868a0c2e01b1b
jarvis211 <kumar.tiwari211@gmail.com>
2021-01-11 22:45:58 +0530

    Part 2: Solution

--- src/test/java/RestaurantServiceTest.java
+++ src/test/java/RestaurantServiceTest.java
@@ -25,12 +25,14 @@ class RestaurantServiceTest {
     @Test
     public void searching_for_existing_restaurant_should_return_expected_restaurant_object() throws restaurantNotFoundException {
         //WRITE UNIT TEST CASE HERE
+        assertEquals(restaurant, service.findRestaurantByName(restaurant.getName()));
     }
 
     //You may watch the video by Muthukumaran on how to write exceptions in Course 3: Testing and Version control: Optional content
     @Test
     public void searching_for_non_existing_restaurant_should_throw_exception() throws restaurantNotFoundException {
         //WRITE UNIT TEST CASE HERE
+        assertThrows(restaurantNotFoundException.class,()->service.findRestaurantByName("Non Existing Restaurant"));
     }
     //<<<<<<<<<<<<<<<<<<<<SEARCHING>>>>>>>>>>>>>>>>>>>>>>>>>>
 
